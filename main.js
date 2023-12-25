1d03391ee2709672031efde1630c7c9e1de5edc3
DonRehan <30264386+donRehan@users.noreply.github.com>
2023-12-25 18:10:21 +0200

    Add random color mode && change cursor on board
    Change cursor shape when on board
    Make change_grid_size affect default_grid_size variable now to access
    it by other methods
    Clear button clears rainbow effect and removes previous BUG
    Make black coloring work by changing div color instead of giving it a
    class and then coloring from the css
    Fix click and drag works in both random colors and in black mode

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ function clear_board() {
 	//Remove all active class from all cells
 	let cells = document.querySelectorAll('.cell');
 	cells.forEach((cell) => {
-		cell.classList.remove('active');
+		cell.style.backgroundColor = 'white';
 	});
+
 }
 
 // Variable to track mouse button state
@@ -18,6 +19,7 @@ let isMouseDown = false;
 function change_grid_size() {
 	let notvalid = true;
 	grid = prompt('Enter the new grid size: ');
+	default_grid_size = grid;
 	while(notvalid){
 		(grid == null || grid =="") || (grid < 1 || grid > 100) ? notvalid = true : notvalid = false;
 		(notvalid) ? grid = prompt('Please enter a valid grid size between 1 and 100') : grid = grid;
@@ -38,26 +40,28 @@ function create_grid(grid_size){
 			let cellDiv = document.createElement('div');
 			cellDiv.classList.add('cell'); // Assign a class for styling
 
-			// Add event listeners for mouseover and mousedown
-			cellDiv.addEventListener('mouseover', function () {
-				if (isMouseDown) {
-					cellDiv.classList.add('active');
-				}
-			});
-			cellDiv.addEventListener('mousedown', function () {
-				isMouseDown = true;
-				cellDiv.classList.add('active');
-			});
-
+			// Prevent BUG of dragging cells 
 			cellDiv.addEventListener('dragstart', function (event) {
 				event.preventDefault(); // Prevent the default drag-and-drop behavior
-			});
-			cellDiv.addEventListener('mouseup', function () {
-				isMouseDown = false;
 			});
 			// Prevent the browser context menu from appearing on right-click
 			cellDiv.addEventListener('contextmenu', function (event) {
 				event.preventDefault();
+			});
+
+			// Add event listeners for mouseover and mousedown
+			cellDiv.addEventListener('mouseover', function (event) {
+				if (event.buttons == 1) 
+				{
+					//Generate a random color and assign it to the cell
+					cellDiv.style.backgroundColor = "black";
+				}
+			});
+
+			// Add event listeners for mousedown and mouseover
+			cellDiv.addEventListener('mousedown', function () {
+				// Generate a random color and assign it to the cell
+				cellDiv.style.backgroundColor = "black";
 			});
 
 			//Add the cell to the row
@@ -73,4 +77,57 @@ function destroy_grid(){
 	while (board.firstChild) {
 		board.removeChild(board.firstChild);
 	}
+
+}
+
+function random_colorize() {
+	/*    Create grid
+		* Add event listener to each cell
+		* When mouseover && mouse pressed , change the background color to a random color
+	*/
+
+	destroy_grid();
+	
+	//TODO : Refactor this code to avoid duplication
+	//This is a copy of create_grid() funcrtion with a random color added
+	for (let row = 0; row < default_grid_size; row++) {
+		let rowDiv = document.createElement('div');
+		board.appendChild(rowDiv);
+		rowDiv.classList.add('row'); // Assign a class for styling
+
+		for (let col = 0; col < default_grid_size; col++) {
+			let cellDiv = document.createElement('div');
+			cellDiv.classList.add('cell'); // Assign a class for styling
+
+			// Prevent BUG of dragging cells 
+			cellDiv.addEventListener('dragstart', function (event) {
+				event.preventDefault(); // Prevent the default drag-and-drop behavior
+			});
+			// Prevent the browser context menu from appearing on right-click
+			cellDiv.addEventListener('contextmenu', function (event) {
+				event.preventDefault();
+			});
+
+			// Add event listeners for mouseover and mousedown
+			cellDiv.addEventListener('mouseover', function (event) {
+				if (event.buttons == 1) 
+				{
+					//Generate a random color and assign it to the cell
+					let random_color = Math.floor(Math.random()*16777215).toString(16);
+					cellDiv.style.backgroundColor = "#" + random_color;
+				}
+			});
+			// Add event listeners for mousedown and mouseover
+			cellDiv.addEventListener('mousedown', function () {
+					//Generate a random color and assign it to the cell
+					let random_color = Math.floor(Math.random()*16777215).toString(16);
+					cellDiv.style.backgroundColor = "#" + random_color;
+			});
+
+
+			//Add the cell to the row
+			rowDiv.appendChild(cellDiv);
+		}
+	}
+
 }
