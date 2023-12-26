6a12dbeb6207c6af1b3d3324586cfe27ce0a2033
DonRehan <30264386+donRehan@users.noreply.github.com>
2023-12-26 03:47:18 +0200

    Finish project && Fix Bugs
    Introduce greyscale mode
    Add black color button
    Fix BUG when clear is pressed => Greyscale mode would not work
    unless button is pressed back for greyscale
    NOW when clear is pressed , if user was using greyscale mode then it
    would continue being greyscale
    Fix BUG when board resize is pressed => color would always turn black
    NOW color becomes whatever was before pessing board change

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 let board = document.getElementById('Board');
 let default_grid_size = 16;
+let current_mode = "black";
 
 
 function clear_board() {
@@ -10,6 +11,10 @@ function clear_board() {
 	cells.forEach((cell) => {
 		cell.style.backgroundColor = 'white';
 	});
+
+	if(current_mode == "grey"){
+		grey_colorize();
+	}
 
 }
 
@@ -26,10 +31,23 @@ function change_grid_size() {
 	}
 	//Clear the grid then create a new one
 	destroy_grid();
-	create_grid(grid);
+	switch(current_mode){
+		case "black":
+		create_grid(default_grid_size);
+		break;
+		case "random":
+		random_colorize();
+		break;
+		case "grey":
+		grey_colorize();
+		break;
+	}
 }
 
 function create_grid(grid_size){
+
+	current_mode = "black";
+	destroy_grid();
 	// Create the 16x16 grid of divs:
 	for (let row = 0; row < grid_size; row++) {
 		let rowDiv = document.createElement('div');
@@ -86,6 +104,7 @@ function random_colorize() {
 		* When mouseover && mouse pressed , change the background color to a random color
 	*/
 
+	current_mode = "random";
 	destroy_grid();
 	
 	//TODO : Refactor this code to avoid duplication
@@ -130,4 +149,58 @@ function random_colorize() {
 		}
 	}
 
+}
+
+function grey_colorize(){
+
+	current_mode = "grey";
+	destroy_grid();
+	
+	//TODO : Refactor this code to avoid duplication
+	//This is a copy of create_grid() funcrtion with a random color added
+	for (let row = 0; row < default_grid_size; row++) {
+		let rowDiv = document.createElement('div');
+		board.appendChild(rowDiv);
+		rowDiv.classList.add('row'); // Assign a class for styling
+
+		for (let col = 0; col < default_grid_size; col++) {
+			let cellDiv = document.createElement('div');
+			cellDiv.classList.add('cell'); // Assign a class for styling
+
+			// Prevent BUG of dragging cells 
+			cellDiv.addEventListener('dragstart', function (event) {
+				event.preventDefault(); // Prevent the default drag-and-drop behavior
+			});
+			// Prevent the browser context menu from appearing on right-click
+			cellDiv.addEventListener('contextmenu', function (event) {
+				event.preventDefault();
+			});
+
+			cellDiv.addEventListener('mousedown', function () {
+				//DO STUFF
+					setOpacity(cellDiv, +0.1);
+			});
+
+			cellDiv.addEventListener('mouseover', function (event) {
+				if (event.buttons == 1) {
+					//Do stuff
+					setOpacity(cellDiv, +0.1);
+				}
+			});
+
+			//Add the cell to the row
+			rowDiv.appendChild(cellDiv);
+		}
+	}
+}
+
+function setOpacity(element, increment) {
+	const computedStyle = window.getComputedStyle(element);
+	const currentColor = computedStyle.getPropertyValue("background-color");
+	if (currentColor.startsWith("rgba")) {
+    const rgbaValues = currentColor.match(/rgba?\((\d+),\s*(\d+),\s*(\d+),\s*(\d+(?:\.\d+)?)\)/);
+    const currentOpacity = parseFloat(rgbaValues[4]);
+    const newOpacity = Math.min(Math.max(currentOpacity + increment, 0), 1); // Ensure opacity stays within 0-1 range
+    element.style.backgroundColor = `rgba(0, 0, 0, ${newOpacity})`;
+	}
 }
